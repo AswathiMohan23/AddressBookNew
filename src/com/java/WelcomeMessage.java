e0d5a931afc7468b906e57d17824caaf9d1f028b
AswathiMohan23 <achu999here@gmail.com>
2022-11-04 09:12:15 +0530

    [ADDED] : welcome message

---
+++ src/com/java/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package com.java;
+
+public class WelcomeMessage {
+    public static void main(String[] args){
+        System.out.println("WELCOME");
+        System.out.println("Continuation of the previous AddressBook repository");
+    }
+}
